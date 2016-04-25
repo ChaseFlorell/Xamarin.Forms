86c151cb3459188a0a5c7f6ff17b3516f54f1ca5
Jason Smith <jason.smith@xamarin.com>
2016-04-25 01:41:20 -0400

    Remove evil chars

--- Xamarin.Forms.Pages.Azure/AzureEasyTableSource.cs
+++ Xamarin.Forms.Pages.Azure/AzureEasyTableSource.cs
@@ -1,45 +1,46 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MobileServices;
 using Newtonsoft.Json.Linq;
-using System.IO;
-​
+
 namespace Xamarin.Forms.Pages.Azure
 {
 	public class AzureEasyTableSource : AzureSource
 	{
 		public static readonly BindableProperty TableNameProperty =
 			BindableProperty.Create(nameof(TableName), typeof(string), typeof(AzureEasyTableSource), null);
-​
+
 		public string TableName
 		{
-			get
-			{
-				return (string)GetValue(TableNameProperty);
-			}
-			set
-			{
-				SetValue(TableNameProperty, value);
-			}
+			get { return (string)GetValue(TableNameProperty); }
+			set { SetValue(TableNameProperty, value); }
 		}
-​
+
 		public override async Task<JToken> GetJson()
 		{
 			var mobileServiceClient = new MobileServiceClient(Uri);
-			var table = mobileServiceClient.GetTable(TableName);
-			var jobj = await GetFromCache();
+			IMobileServiceTable table = mobileServiceClient.GetTable(TableName);
+			JToken jobj = await GetFromCache();
 			if (jobj != null)
 				return jobj;
 			jobj = await table.ReadAsync(string.Empty);
 			await WriteToCache(jobj);
 			return jobj;
 		}
-​
+
+		Task<Stream> GetFileCacheStream()
+		{
+			IIsolatedStorageFile store = Device.PlatformServices.GetUserStoreForApplication();
+			Task<Stream> file = store.OpenFileAsync("storedCode", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+			return file;
+		}
+
 		async Task<JToken> GetFromCache()
 		{
 			try
 			{
-				var file = await GetFileCacheStream();
+				Stream file = await GetFileCacheStream();
 				var writer = new StreamReader(file);
 				return await writer.ReadToEndAsync();
 			}
@@ -48,28 +49,18 @@ namespace Xamarin.Forms.Pages.Azure
 				return null;
 			}
 		}
-​
+
 		async Task WriteToCache(JToken token)
 		{
 			try
 			{
-​
-				var file = await GetFileCacheStream();
+				Stream file = await GetFileCacheStream();
 				var writer = new StreamWriter(file);
 				await writer.WriteAsync(token.ToString());
 			}
 			catch (Exception ex)
 			{
-​
 			}
 		}
-​
-		Task<System.IO.Stream> GetFileCacheStream()
-		{
-			var store = Device.PlatformServices.GetUserStoreForApplication();
-			var file = store.OpenFileAsync("storedCode", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-			return file;
-		}
-​
 	}
 }
